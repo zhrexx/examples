301321234bfd8e3b2264cb49048e4c696be32ed5
dev <zhrexx@users.noreply.github.com>
2025-05-13 20:12:18 +0200

    aboba

--- zig/Threads/thread.zig
+++ zig/Threads/thread.zig
@@ -2,11 +2,14 @@ const std = @import("std");
 
 fn example_thread_fn(context: *u32, aboba: u8) void {
     std.debug.print("Hello from thread! Value = {} {}\n", .{context.*, aboba});
+    std.time.sleep(5 * std.time.ns_per_s);
+    std.debug.print("Woke up\n", .{});
 }
 
 pub fn main() u8 {
     var value: u32 = 1000;
     const handle = std.Thread.spawn(.{}, example_thread_fn, .{&value, 10}) catch @panic("could not create thread");
+    // here actions in main thread
     handle.join();
     return 0;
 }
