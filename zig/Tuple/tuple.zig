892f3dd2dca5f920748e3863be9fee4624ca2b28
dev <zhrexx@users.noreply.github.com>
2025-05-13 19:50:05 +0200

    aboba

--- zig/Tuple/tuple.zig
+++ zig/Tuple/tuple.zig
@@ -1,13 +1,15 @@
 const std = @import("std");
 
-fn print_args(values: anytype) void {
-    inline for (values) |val| {
-        std.debug.print("{} ", .{val});
+fn iterate_over_tuple(tuple: anytype) void {
+    var i: u64 = 0;
+    //const tuple_len = @typeInfo(@TypeOf(tuple)).Struct.fields.len;
+    inline for (tuple) |item| {
+        std.debug.print("{d} {}\n", .{i, item});
+        i += 1;
+        //if (tuple_len > i) std.debug.print("lul {} items left \n", .{tuple_len - i});
     }
-    std.debug.print("\n", .{});
 }
 
 pub fn main() void {
-    print_args(.{1, 2, 3, 4});
+    iterate_over_tuple(.{1, 2, 3, 4});
 }
-
