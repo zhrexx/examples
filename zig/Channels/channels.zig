6ecd761f5f49560808b01db5d8144cbf8b7f9676
dev <zhrexx@users.noreply.github.com>
2025-06-06 17:14:57 +0200

    abc

--- zig/Channels/channels.zig
+++ zig/Channels/channels.zig
@@ -211,3 +211,75 @@ pub fn createBoundedChannel(comptime T: type, allocator: Allocator, capacity: us
 pub fn createSynchronousChannel(comptime T: type, allocator: Allocator) Channel(T) {
     return Channel(T).init(allocator, 0);
 }
+
+test "String Channel" {
+    const allocator = std.heap.page_allocator;
+    var channel = createBoundedChannel([]const u8, allocator, 5);
+    defer channel.deinit();
+
+    const producer = try std.Thread.spawn(.{}, struct {
+        fn threadFn(chan: *Channel([]const u8)) void {
+            const aboba: []const u8 = "Hello, World";
+            for (0..10) |i|{
+                _ = i;
+                chan.send(aboba) catch |err| {
+                    std.debug.print("Producer error: {}\n", .{err});
+                    return;
+                };
+                std.debug.print("Sent: {s}\n", .{aboba});
+                std.time.sleep(50 * std.time.ns_per_ms);
+            }
+            chan.close();
+        }
+    }.threadFn, .{&channel});
+
+    while (true) {
+        const value = channel.receive() catch |err| {
+            if (err == Channel([]const u8).Error.Closed) {
+                std.debug.print("Channel closed, exiting\n", .{});
+                break;
+            }
+            std.debug.print("Consumer error: {}\n", .{err});
+            break;
+        };
+        std.debug.print("Received: {s}\n", .{value});
+        std.time.sleep(100 * std.time.ns_per_ms);
+    }
+
+    producer.join();
+}
+
+test "Integer Channel" {
+    const allocator = std.heap.page_allocator;
+    var channel = createBoundedChannel(i32, allocator, 5);
+    defer channel.deinit();
+
+    const producer = try std.Thread.spawn(.{}, struct {
+        fn threadFn(chan: *Channel(i32)) void {
+            for (0..10) |i| {
+                chan.send(@intCast(i)) catch |err| {
+                    std.debug.print("Producer error: {}\n", .{err});
+                    return;
+                };
+                std.debug.print("Sent: {}\n", .{i});
+                std.time.sleep(50 * std.time.ns_per_ms);
+            }
+            chan.close();
+        }
+    }.threadFn, .{&channel});
+
+    while (true) {
+        const value = channel.receive() catch |err| {
+            if (err == Channel(i32).Error.Closed) {
+                std.debug.print("Channel closed, exiting\n", .{});
+                break;
+            }
+            std.debug.print("Consumer error: {}\n", .{err});
+            break;
+        };
+        std.debug.print("Received: {}\n", .{value});
+        std.time.sleep(100 * std.time.ns_per_ms);
+    }
+
+    producer.join();
+}
